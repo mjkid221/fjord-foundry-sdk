831d8bb72886feeec1962b1c9851ca95d8b864d1
Simon Tanna <92503409+simon-tanna@users.noreply.github.com>
2024-04-04 11:54:00 +1000

    feat: ✨ create basic config for packages and testts

---
+++ packages/client/src/types/index.ts
@@ -0,0 +1,3 @@
+export type HelloWorld = {
+  message: string;
+};
---
+++ tests/setup/setup.ts
@@ -0,0 +1,8 @@
+beforeAll(async () => {
+  /**
+   * Emulate browser environment
+   */
+  globalThis.self = globalThis as any;
+});
+
+afterAll(async () => {});
